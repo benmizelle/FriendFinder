86dea49e6ad2c8f8266d476bbe9d9c4824b20de1
Ben Mizelle <Ben Mizelle>
2017-04-19 18:25:49 -0400

    save for heroku deployment

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,53 @@
 // setting up the friends array of objects
 var friendsArray = [
-
-
+    {
+        "name": "Harry",
+        "photo": "https://tse3.mm.bing.net/th?id=OIP.c45O2P2ntRqtSzHpE-MeowELEs&pid=15.1",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Ron",
+        "photo": "https://tse4.mm.bing.net/th?id=OIP.FtznGuEWWWUaKBIUrTIAWAEsDh&pid=15.1",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Hermione",
+        "photo": "https://images2.fanpop.com/images/photos/7200000/hermione-hermione-granger-7293174-1794-2200.jpg",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
 ];
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,7 +15,8 @@ module.exports = function(app) {
     // allows user to post data to the api
     app.post("/api/friends", function(req, res) {
 
-
+        friendsData.push(req.body);
+        res.json(true);
 
     });
 
