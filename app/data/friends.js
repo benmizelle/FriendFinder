82daee7117d9fb72d1855fc764d3e531d3c38efe
Ben Mizelle <Ben Mizelle>
2017-04-19 22:39:53 -0400

    coming to a close

--- app/data/friends.js
+++ app/data/friends.js
@@ -48,6 +48,86 @@ var friendsArray = [
             "1"
         ]
     },
+    {
+        "name": "Malfoy",
+        "photo": "http://cdn.movieweb.com/img.news.tops/NEh934QRmEyekk_2_b.jpg",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Voldemort",
+        "photo": "https://tse1.mm.bing.net/th?id=OIP.7HQWOlY8EIjFciT4ua4qKAEsDZ&pid=15.1",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Bellatrix",
+        "photo": "https://tse1.mm.bing.net/th?id=OIP.NWiTkA38jB_F_5eVW3dRtgDjEs&pid=15.11",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Ginny",
+        "photo": "http://images5.fanpop.com/image/photos/25000000/h-g-beauty-ginevra-ginny-weasley-25005642-1874-2500.jpg",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Luna",
+        "photo": "https://tse3.mm.bing.net/th?id=OIP.SLPS4HtkvzVslzqWb6ACCQEsDh&w=267&h=200&c=7&qlt=90&o=4&dpr=1.25&pid=1.7",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
 ];
 
 module.exports = friendsArray;
